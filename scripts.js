c74e99dae1a02d554f00b4ee1194055eda4b4321
Reyhaneh MH <reyhanehosseini@gmail.com>
2024-08-12 13:14:10 -0400

    css comments removed, added load state, div for stats created

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+document.onreadystatechange = function () {
+  var state = document.readyState;
+  if (state !== "complete") {
+    document.getElementById("content").style.visibility = "hidden";
+  } else if (state == "complete") {
+    setTimeout(function () {
+      document.getElementById("loader").style.visibility = "hidden";
+      document.getElementById("content").style.visibility = "visible";
+    }, 1000);
+  }
+};
+
 async function getMultiplePokemonData() {
   const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
   const numberOfPokemons = 30; // Number of Pokémon data you want to fetch
@@ -51,6 +63,7 @@ function displayPokemonData(dataArray) {
     card.innerHTML = `#${index + 1}<br>`;
     container.appendChild(card);
 
+    // Add Pokémon image
     const image = document.createElement("img");
     image.src = `${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"}${
       index + 1
@@ -59,9 +72,31 @@ function displayPokemonData(dataArray) {
     image.width = 120;
     card.appendChild(image);
 
-    const pokemonIdSpan = document.createElement("span");
-    pokemonIdSpan.innerHTML = `<h2>${name}</h2> Weight &nbsp;&nbsp;&nbsp; Height <br> <b>${weight}kg</b> <b>${height}m</b> <br> <br> Type: <b>${type1}${type2}</b>`;
-    card.appendChild(pokemonIdSpan);
+    // Add Pokémon name
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = name;
+    card.appendChild(nameElement);
+
+    // Add Pokémon weight and height side by side
+    const statsContainer = document.createElement("div");
+    statsContainer.className = "stats-container";
+
+    const weightDiv = document.createElement("div");
+    weightDiv.className = "stat";
+    weightDiv.innerHTML = `<div class="stat-label">Weight</div><div class="stat-value">${weight} kg</div>`;
+    statsContainer.appendChild(weightDiv);
+
+    const heightDiv = document.createElement("div");
+    heightDiv.className = "stat";
+    heightDiv.innerHTML = `<div class="stat-label">Height</div><div class="stat-value">${height} m<br></br></div>`;
+    statsContainer.appendChild(heightDiv);
+
+    card.appendChild(statsContainer);
+
+    // Add Pokémon type
+    const typeElement = document.createElement("div");
+    typeElement.innerHTML = `Type: <b>${type1}${type2}</b>`;
+    card.appendChild(typeElement);
   });
 }
 
